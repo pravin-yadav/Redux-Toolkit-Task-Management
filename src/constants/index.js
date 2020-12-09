683fc02efbc0b5166d60289155413eb2296b5eed
pravin-yadav <work.coderpravin@gmail.com>
2020-12-09 23:24:57 +0530

    Added constants for task management.

---
+++ src/constants/index.js
@@ -0,0 +1,6 @@
+export const CREATE_TASK = 'CREATE_TASK';
+export const UPDATE_TASK = 'UPDATE_TASK';
+export const DELETE_TASK = 'DELETE_TASK';
+export const ACTIVE_TASK = 'ACTIVE_TASK';
+export const RESET_TASK = 'RESET_TASK';
+
